7e0ef8ea4728a31643ee24fb847cf2835df168c9
jamessouth <jamesdanielsouth@gmail.com>
2026-08-28 01:47:57 -0500

    move to subcommands

--- chyron/bin/main.ml
+++ chyron/bin/main.ml
@@ -2,40 +2,15 @@ open Core
 open Chyron
 (* open Core_bench *)
 
-let flags : cliflags Command.Param.t =
+let uflags : universalflags Command.Param.t =
   let%map_open.Command cycles =
     flag_optional_with_default_doc "--cycles" ~aliases:[ "-c" ] Ints.zeroplus
       (fun x -> Int.sexp_of_t x)
       ~default:65_536 ~doc:"int number of scroll cycles of TEXT\n"
-  and direction =
-    flag_optional_with_default_doc "--direction" ~aliases:[ "-d" ] Direction.arg
-      Direction.sexp_of_t ~default:Left
-      ~doc:"string scroll TEXT left, right, or bounce\n"
-  and endcap_char =
-    flag_optional_with_default_doc "--endcap-char" ~aliases:[ "-ec" ] char
-      (fun x -> Char.sexp_of_t x)
-      ~default:' ' ~doc:"char endcap between end and start of TEXT\n"
-  and endcap_len =
-    flag_optional_with_default_doc "--endcap-len" ~aliases:[ "-el" ]
-      Ints.oneplus
-      (fun x -> Int.sexp_of_t x)
-      ~default:1 ~doc:"int minimum length of endcap\n"
-  and mode =
-    flag_optional_with_default_doc "--mode" ~aliases:[ "-m" ] Mode.arg
-      Mode.sexp_of_t ~default:Wrap
-      ~doc:"string reset TEXT, wrap around, or split-flap\n"
   and prefix =
     flag_optional_with_default_doc "--prefix" ~aliases:[ "-p" ] string
       (fun x -> String.sexp_of_t x)
       ~default:"" ~doc:"string prefix TEXT at left of display\n"
-  and rest =
-    flag_optional_with_default_doc "--rest" ~aliases:[ "-r" ] Ints.zeroplus
-      (fun x -> Int.sexp_of_t x)
-      ~default:0 ~doc:"int additional sleep in ms for frames at extremes\n"
-  and scroll =
-    flag_optional_with_default_doc "--scroll" ~aliases:[ "-sc" ] Scroll.arg
-      Scroll.sexp_of_t ~default:Char
-      ~doc:"string scroll TEXT by character or by word\n"
   and sleep =
     flag_optional_with_default_doc "--sleep" ~aliases:[ "-sl" ] Ints.oneplus
       (fun x -> Int.sexp_of_t x)
@@ -53,28 +28,61 @@ let flags : cliflags Command.Param.t =
       (fun x -> Int.sexp_of_t x)
       ~default:15 ~doc:"int display width of TEXT, exclusive of {pre,suf}fix\n"
   in
-  {
-    cycles;
-    direction;
-    endcap_char;
-    endcap_len;
-    rest;
-    mode;
-    prefix;
-    scroll;
-    sleep;
-    suffix;
-    terminator;
-    width;
-  }
+  { cycles; prefix; sleep; suffix; terminator; width }
+
+let bflags : bounceflags Command.Param.t =
+  let%map_open.Command endcap_char =
+    flag_optional_with_default_doc "--endcap-char" ~aliases:[ "-ec" ] char
+      (fun x -> Char.sexp_of_t x)
+      ~default:' ' ~doc:"char endcap between end and start of TEXT\n"
+  and endcap_len =
+    flag_optional_with_default_doc "--endcap-len" ~aliases:[ "-el" ]
+      Ints.oneplus
+      (fun x -> Int.sexp_of_t x)
+      ~default:1 ~doc:"int minimum length of endcap\n"
+  and rest =
+    flag_optional_with_default_doc "--rest" ~aliases:[ "-r" ] Ints.zeroplus
+      (fun x -> Int.sexp_of_t x)
+      ~default:0 ~doc:"int additional sleep in ms for frames at extremes\n"
+  and scroll_unit =
+    flag_optional_with_default_doc "--scroll-unit" ~aliases:[ "-sc" ]
+      Scroll_unit.arg Scroll_unit.sexp_of_t ~default:Char
+      ~doc:"string scroll TEXT by character or by word\n"
+  in
+  { endcap_char; endcap_len; rest; scroll_unit }
+
+let sflags : scrollflags Command.Param.t =
+  let%map_open.Command direction =
+    flag_optional_with_default_doc "--direction" ~aliases:[ "-d" ] Direction.arg
+      Direction.sexp_of_t ~default:Left
+      ~doc:"string scroll TEXT left, right, or bounce\n"
+  and scroll_mode =
+    flag_optional_with_default_doc "--scroll-mode" ~aliases:[ "-m" ]
+      Scroll_mode.arg Scroll_mode.sexp_of_t ~default:Wrap
+      ~doc:"string reset TEXT, wrap around, or split-flap\n"
+  in
+  { direction; scroll_mode }
+
+let scroll =
+  Command.basic ~summary:"scroll mode summary"
+    ~readme:(fun () -> "scroll mode details")
+    (let%map_open.Command text =
+       anon (non_empty_sequence_as_list ("text" %: string))
+     and uflags
+     and sflags
+     and bflags in
+     fun () -> run_scroll text uflags sflags bflags)
+
+let bounce =
+  Command.basic ~summary:"bounce mode summary"
+    ~readme:(fun () -> "bounce mode details")
+    (let%map_open.Command text =
+       anon (non_empty_sequence_as_list ("text" %: string))
+     and uflags
+     and bflags in
+     fun () -> run_bounce text uflags bflags)
 
 let () =
-  let summ = "write me" in
-  let mdi = "write me with more details" in
   Command_unix.run ~version:"1.0" ~build_info:"tbd"
-    (Command.basic ~summary:summ
-       ~readme:(fun () -> mdi)
-       (let%map_open.Command text =
-          anon (non_empty_sequence_as_list ("text" %: string))
-        and flags in
-        fun () -> run text flags))
+    (Command.group ~summary:"group summary"
+       [ ("bounce", bounce); ("scroll", scroll) ])
--- chyron/lib/chyron.ml
+++ chyron/lib/chyron.ml
@@ -1,12 +1,12 @@
 open Core
 
-module Mode = struct
-  type t = Reset | Split_flap | Wrap [@@deriving sexp]
+module Scroll_mode = struct
+  type t = Reset | Wrap [@@deriving sexp]
 
   let arg =
     Command.Arg_type.of_alist_exn ~accept_unique_prefixes:true
       ~case_sensitive:false ~list_values_in_help:false
-      [ ("reset", Reset); ("split-flap", Split_flap); ("wrap", Wrap) ]
+      [ ("reset", Reset); ("wrap", Wrap) ]
 end
 
 module Ints = struct
@@ -20,7 +20,7 @@ module Ints = struct
   let twoplus = Command.Arg_type.create (parseint ~min:2)
 end
 
-module Scroll = struct
+module Scroll_unit = struct
   type t = Char | Word [@@deriving sexp]
 
   let arg =
@@ -54,44 +54,34 @@ module Externs = struct
 end
 
 module Direction = struct
-  type t = Bounce | Left | Right [@@deriving equal, sexp]
+  type t = Left | Right [@@deriving sexp]
 
   let arg =
     Command.Arg_type.of_alist_exn ~accept_unique_prefixes:true
       ~case_sensitive:false ~list_values_in_help:false
-      [ ("bounce", Bounce); ("left", Left); ("right", Right) ]
+      [ ("left", Left); ("right", Right) ]
 end
 
-type cliflags = {
+type universalflags = {
   cycles : int;
-  direction : Direction.t;
-  endcap_char : char;
-  endcap_len : int;
-  rest : int;
-  mode : Mode.t;
   prefix : string;
-  scroll : Scroll.t;
   sleep : int;
   suffix : string;
   terminator : Terminator.t;
   width : int;
 }
 
-let run text
-    {
-      cycles;
-      direction;
-      endcap_char;
-      endcap_len;
-      rest;
-      mode;
-      prefix;
-      scroll;
-      sleep;
-      suffix;
-      terminator;
-      width;
-    } =
+type bounceflags = {
+  endcap_char : char;
+  endcap_len : int;
+  rest : int;
+  scroll_unit : Scroll_unit.t;
+}
+
+type scrollflags = { direction : Direction.t; scroll_mode : Scroll_mode.t }
+
+let run_scroll text { cycles; prefix; sleep; suffix; terminator; width }
+    { direction; scroll_mode } { endcap_char; endcap_len; rest; scroll_unit } =
   let vclen_charlist str =
     let tl, cl =
       Uuseg_string.fold_utf_8 `Grapheme_cluster
@@ -105,20 +95,17 @@ let run text
   let visual_chars, _ = vclen_charlist joined_text in
   let width_minus_visual_chars = width - visual_chars in
   let ecl =
-    if Direction.equal direction Bounce then Int.max 0 width_minus_visual_chars
-    else
-      Int.clamp_exn
-        (Int.max endcap_len width_minus_visual_chars)
-        ~min:1 ~max:(pred width)
+    Int.clamp_exn
+      (Int.max endcap_len width_minus_visual_chars)
+      ~min:1 ~max:(pred width)
   in
   let ecp = Bytes.make ecl endcap_char in
   let joined_bytes = Bytes.of_string joined_text in
-  let bounce = Stdlib.Bytes.cat (Stdlib.Bytes.cat ecp joined_bytes) ecp in
+  let base = Stdlib.Bytes.cat (Stdlib.Bytes.cat ecp joined_bytes) ecp in
   let finaltext =
     match direction with
-    | Bounce -> bounce
-    | Left -> Stdlib.Bytes.cat joined_bytes bounce
-    | Right -> Stdlib.Bytes.cat bounce joined_bytes
+    | Left -> Stdlib.Bytes.cat joined_bytes base
+    | Right -> Stdlib.Bytes.cat base joined_bytes
   in
   let lastchar =
     match terminator with Newline -> '\n' | Return -> '\r' | Space -> ' '
@@ -140,24 +127,13 @@ let run text
     let pwlen = List.length pwlist in
     let tot = sleep + rest in
     let pwslist =
-      match (direction, mode) with
-      | Bounce, _ ->
-          let maxpos =
-            let rec loop max = function
-              | [] -> max
-              | (p, _) :: t -> loop (if p > max then p else max) t
-            in
-            loop 0 pwlist
-          in
-          List.map pwlist ~f:(fun (p, w) ->
-              if p = 0 || p = maxpos then (p, w, tot) else (p, w, sleep))
-      | _, Wrap ->
+      match scroll_mode with
+      | Wrap ->
           List.mapi pwlist ~f:(fun i (p, w) ->
               if i = 0 then (p, w, tot) else (p, w, sleep))
-      | _, Reset ->
+      | Reset ->
           List.mapi pwlist ~f:(fun i (p, w) ->
               if i = 0 || i = pred pwlen then (p, w, tot) else (p, w, sleep))
-      | _ -> []
     in
     let flatlist =
       let rec loop = function
@@ -225,24 +201,11 @@ let run text
   let rchar = ucinds false revcharlist (fun _ -> 1) accmfn in
   let takeappend r l = List.append l (List.take r 1) in
   begin match
-    (direction, scroll, mode, Ordering.of_int (compare visual_chars width))
+    ( direction,
+      scroll_unit,
+      scroll_mode,
+      Ordering.of_int (compare visual_chars width) )
   with
-  | _, _, Split_flap, _ -> print_endline "split-flap"
-  | Bounce, Char, (Wrap | Reset), (Greater | Equal | Less) ->
-      let l, r = List.split_while blchar ~f:(fun (a, b) -> a + b < totallen) in
-      List.append (takeappend r l) (List.rev (List.drop l 1)) |> loopandprint
-  | Bounce, Word, (Wrap | Reset), Greater -> begin
-      let fltr =
-        List.filteri (List.append blword brword) ~f:(fun i (a, _) ->
-            (a > 0 || i = 0) && a <= lenminuswidth)
-      in
-      List.remove_consecutive_duplicates fltr ~equal:(fun (a, _) (b, _) ->
-          a = b)
-      |> loopandprint
-    end
-  | Bounce, Word, (Wrap | Reset), (Equal | Less) ->
-      [ (0, jointextlen + ecl); (lenminuswidth, jointextlen + ecl) ]
-      |> loopandprint
   | Left, Char, Reset, Greater ->
       List.filter blchar ~f:(fun (a, b) -> a + b <= halflen - ecl)
       |> loopandprint
@@ -275,6 +238,140 @@ let run text
     end
   | Right, Word, Wrap, (Greater | Equal | Less) ->
       List.take brword (List.fold text ~init:0 ~f:(fun i _ -> succ i))
+      |> loopandprint
+  end;
+  match terminator with
+  | Newline -> ()
+  | _ ->
+      Externs.unsafe_output_char stdout '\n';
+      Externs.unsafe_flush stdout
+
+let run_bounce text { cycles; prefix; sleep; suffix; terminator; width }
+    { endcap_char; endcap_len; rest; scroll_unit } =
+  let vclen_charlist str =
+    let tl, cl =
+      Uuseg_string.fold_utf_8 `Grapheme_cluster
+        (fun (tlacc, clacc) char -> (succ tlacc, char :: clacc))
+        (0, []) str
+    in
+    (tl, cl)
+  in
+  let joined_text = String.concat ~sep:" " text in
+  let jointextlen = String.length joined_text in
+  let visual_chars, _ = vclen_charlist joined_text in
+  let width_minus_visual_chars = width - visual_chars in
+  let ecl = Int.max 0 width_minus_visual_chars in
+  let ecp = Bytes.make ecl endcap_char in
+  let joined_bytes = Bytes.of_string joined_text in
+  let finaltext = Stdlib.Bytes.cat (Stdlib.Bytes.cat ecp joined_bytes) ecp in
+  let lastchar =
+    match terminator with Newline -> '\n' | Return -> '\r' | Space -> ' '
+  in
+  let pfix = Bytes.of_string prefix in
+  let plen = Bytes.length pfix in
+  let sfix = Bytes.of_string suffix in
+  let slen = Bytes.length sfix in
+  let print pos wid =
+    (* print_endline (string_of_int pos);
+    print_endline (string_of_int wid); *)
+    Externs.unsafe_output_bytes stdout pfix 0 plen;
+    Externs.unsafe_output_bytes stdout finaltext pos wid;
+    Externs.unsafe_output_bytes stdout sfix 0 slen;
+    Externs.unsafe_output_char stdout lastchar;
+    Externs.unsafe_flush stdout
+  in
+  let loopandprint pwlist =
+    let pwlen = List.length pwlist in
+    let tot = sleep + rest in
+    let pwslist =
+      let maxpos =
+        let rec loop max = function
+          | [] -> max
+          | (p, _) :: t -> loop (if p > max then p else max) t
+        in
+        loop 0 pwlist
+      in
+      List.map pwlist ~f:(fun (p, w) ->
+          if p = 0 || p = maxpos then (p, w, tot) else (p, w, sleep))
+    in
+    let flatlist =
+      let rec loop = function
+        | [] -> []
+        | (p, w, s) :: t -> p :: w :: s :: loop t
+      in
+      loop pwslist
+    in
+    print_endline (List.to_string ~f:string_of_int flatlist);
+    print_endline (Bytes.to_string finaltext);
+    let indexes = Array.of_list flatlist in
+    let arrlen = Array.length indexes - 3 in
+    let rec loop ticks idx =
+      if ticks <= 0 then ()
+      else begin
+        print
+          (Array.unsafe_get indexes idx)
+          (Array.unsafe_get indexes (succ idx));
+        Externs.caml_clock_nanosleep (Array.unsafe_get indexes (idx + 2));
+        let nidx = if idx = arrlen then 0 else idx + 3 in
+        (loop [@tailcall]) (pred ticks) nidx
+      end
+    in
+    loop (pwlen * cycles) 0
+  in
+  let bytesofutfchars str visualchars =
+    let bytelen, _ =
+      Uuseg_string.fold_utf_8 `Grapheme_cluster
+        (fun (bytecount, charcount) char ->
+          if charcount >= visualchars then (bytecount, charcount)
+          else (bytecount + String.length char, succ charcount))
+        (0, 0) str
+    in
+    bytelen
+  in
+  let _, charlist = vclen_charlist (Bytes.to_string finaltext) in
+  let revcharlist = List.rev charlist in
+  let totallen = Bytes.length finaltext in
+  let lenminuswidth =
+    totallen - bytesofutfchars (String.concat charlist) width
+  in
+  let ucinds rev cl sptfn accfn =
+    let rec loop pos acc = function
+      | [] -> if rev then List.rev acc else acc
+      | h :: t ->
+          let lt = h :: t in
+          let str = String.concat lt in
+          let bts = bytesofutfchars str width in
+          let l, r = List.split_n lt (sptfn lt) in
+          loop (String.length (String.concat l) + pos) (accfn str bts pos acc) r
+    in
+    loop 0 [] cl
+  in
+  let wordsplitfn chr =
+    succ (List.length (List.take_while chr ~f:(fun s -> String.( <> ) s " ")))
+  in
+  let accmfn _ bts pos acc = (pos, bts) :: acc in
+  let brword =
+    ucinds true charlist wordsplitfn (fun str bts _ acc ->
+        (String.length str - bts, bts) :: acc)
+  in
+  let blchar = ucinds true revcharlist (fun _ -> 1) accmfn in
+  let blword = ucinds true revcharlist wordsplitfn accmfn in
+  let takeappend r l = List.append l (List.take r 1) in
+  begin match (scroll_unit, Ordering.of_int (compare visual_chars width)) with
+  | Char, (Greater | Equal | Less) ->
+      let l, r = List.split_while blchar ~f:(fun (a, b) -> a + b < totallen) in
+      List.append (takeappend r l) (List.rev (List.drop l 1)) |> loopandprint
+  | Word, Greater -> begin
+      let fltr =
+        List.filteri (List.append blword brword) ~f:(fun i (a, _) ->
+            (a > 0 || i = 0) && a <= lenminuswidth)
+      in
+      List.remove_consecutive_duplicates fltr ~equal:(fun (a, _) (b, _) ->
+          a = b)
+      |> loopandprint
+    end
+  | Word, (Equal | Less) ->
+      [ (0, jointextlen + ecl); (lenminuswidth, jointextlen + ecl) ]
       |> loopandprint
   end;
   match terminator with
